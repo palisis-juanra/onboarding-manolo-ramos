1bd0f1cf9dc2c02c8fa97c42cf009e78f6474f49
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-16 16:09:42 +0200

    TOURCMS-11745 wip app routing

--- src/core/App.php
+++ src/core/App.php
@@ -1,10 +1,49 @@
 <?php
+
 namespace Core;
 
-class App 
+use Services\TemplateRenderService;
+
+class App
 {
-	public function run() 
+	private $routes = [
+		'/login' => [
+			[
+				'controller' => 'LoginController',
+				'method' => 'POST',
+				'action' => 'login',
+			],
+			[
+				'controller' => 'LoginController',
+				'method' => 'GET',
+				'action' => 'loginPage',
+			],
+		],
+		'/logout' => [
+			[
+				'controller' => 'LoginController',
+				'method' => 'GET',
+				'action' => 'logout',
+			],
+		]
+	];
+
+	public function __construct()
 	{
-		echo "<p>Application is running!</p>";
+		// Initialize any necessary components or services here
 	}
+
+	public function run()
+	{
+		// Call the controller -> use the template service
+		$templateRender = new TemplateRenderService();
+		$templateRender->renderTemplate('login/loginPage', []);
+	}
+
+	private function isLoggedIn(): bool
+	{
+		// Check redis
+		return true;
+	}
+
 }
