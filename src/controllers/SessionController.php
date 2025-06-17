e37334cf72a90e4f90e118b5e30f4662b37abaab
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-17 16:58:38 +0200

    TOURCMS-11745 WIP redis integration in sessionController

--- src/controllers/SessionController.php
+++ src/controllers/SessionController.php
@@ -23,9 +23,27 @@ class SessionController
 			$redisConfig['REDIS_PASSWORD']
 		);
 
-		$this->templateRenderer = new TemplateRendererService();
+		// Start the session or create a new one if it doesn't exist
+		self::createSession();
 
-		session_start();
+		$this->templateRenderer = new TemplateRendererService();
+	}
+	
+	private function createSession() {
+		if ($this->redisClient->getItemFromRedis('session_key', RedisInstanceHelper::REDIS_TYPE_STRING)) {
+			// If session key exists, check if the session is still valid
+			if (time() - $this->redisClient->getItemFromRedis('session_key', RedisInstanceHelper::REDIS_TYPE_STRING) < self::SESSION_TTL) {
+				// Session is valid, continue
+				session_start();
+				return;
+			} else {
+				// Session expired, clear the session key
+				$this->redisClient->deleteItemFromRedis('session_key', RedisInstanceHelper::REDIS_TYPE_STRING);
+			}
+		}
+
+		// Store an unique session key in Redis
+		$this->redisClient->storeItemInRedis('session_key', bin2hex(random_bytes(16)), RedisInstanceHelper::REDIS_TYPE_STRING);
 	}
 
 	// TODO: redis, redir if logged, handle submit
@@ -52,16 +70,22 @@ class SessionController
 	 */
 	public function logOut()
 	{
+		//TODO: Implement the logout logic here
 		$this->templateRenderer->renderTemplate('_common/logoutPage', []);
 	}
 
+	/**
+	 * Checks if the user is logged in.
+	 *
+	 * This method checks the Redis session to determine if the user is logged in.
+	 * If not, it redirects to the login page.
+	 *
+	 * @return void
+	 */
 	private function redirectLogin()
 	{
 		header('Location: /login');
 		exit;
 	}
 
-	public function createSessionObject() {
-
-	}
 }
