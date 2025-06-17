2828988b369d1f5f63a4329f0b1561e7856e8969
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-17 16:18:34 +0200

    TOURCMS-11745 updated the session controller to handle logIn and logOut logic, init redis config

--- src/controllers/SessionController.php
+++ src/controllers/SessionController.php
@@ -2,16 +2,30 @@
 
 namespace Controllers;
 
-use Services\TemplateRenderService;
+require_once __DIR__ . '/../../config/env_config.php';
+
+use Services\TemplateRendererService;
+use Helpers\RedisInstanceHelper;
 
 class SessionController
 {
-	// Initialize the TemplateRenderService
-	private $templateRender;
+	private $templateRenderer;
 
-	public function __construct()
+	protected $redisClient;
+	protected const SESSION_TTL = 1800; // 30 minutes
+
+	public function __construct(array $redisConfig)
 	{
-		$this->templateRender = new TemplateRenderService();
+		// Redis client initialization
+		$this->redisClient = RedisInstanceHelper::getInstance(
+			$redisConfig['REDIS_HOST'],
+			$redisConfig['REDIS_PORT'],
+			$redisConfig['REDIS_PASSWORD']
+		);
+
+		$this->templateRenderer = new TemplateRendererService();
+
+		session_start();
 	}
 
 	// TODO: redis, redir if logged, handle submit
@@ -25,11 +39,29 @@ class SessionController
 	 */
 	public function logIn()
 	{
-		$this->templateRender->renderTemplate('login/loginPage', []);
+		$this->templateRenderer->renderTemplate('login/loginPage', []);
 	}
 
+	/**
+	 * Handles the logout request.
+	 *
+	 * This method processes the logout request, clears the session,
+	 * and returns a response indicating successful logout.
+	 *
+	 * @return void
+	 */
 	public function logOut()
 	{
-		$this->templateRender->renderTemplate('_common/logoutPage', []);
+		$this->templateRenderer->renderTemplate('_common/logoutPage', []);
+	}
+
+	private function redirectLogin()
+	{
+		header('Location: /login');
+		exit;
+	}
+
+	public function createSessionObject() {
+
 	}
 }
