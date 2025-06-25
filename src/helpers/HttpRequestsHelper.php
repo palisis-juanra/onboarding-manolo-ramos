c96f47c9587db7dabfc8dd1162d12ae2f6c6414d
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-25 13:02:27 +0200

    TOURCMS-11746 update function names, add compareroutehttp method

--- src/helpers/HttpRequestsHelper.php
+++ src/helpers/HttpRequestsHelper.php
@@ -20,7 +20,7 @@ class HttpRequestsHelper
 	 * @param string $verb The verb to be matched against the known verbs list
 	 * @return string $matchingVerb The matched verb
 	 */
-	public static function getVerbs(string $verb): string
+	public static function getVerb(string $verb): string
 	{
 		$matchingVerb = null;
 		$uppercaseVerb = strtoupper($verb);
@@ -41,8 +41,27 @@ class HttpRequestsHelper
 	 * @param string $verb The verb to be checked
 	 * @return bool returns the result of the comprobation
 	 */
-	public static function isValid(string $verb): bool
+	public static function checkHttpMethod(string $verb): bool
 	{
 		return in_array($verb, self::$httpVerbs);
+	}
+
+	/**
+	 * Compares the HTTP method used in the request with the method defined in the route.
+	 *
+	 * @param string $routeDefinedMethod The HTTP method defined for the route.
+	 * @param string $httpRequestMethodUsed The HTTP method used in the request.
+	 * @return bool True if they match, false otherwise.
+	 */
+	public static function compareRouteHttpMethodUsed(
+		string $routeDefinedMethod, 
+		string $httpRequestMethodUsed
+	): bool
+	{
+		// Convert to uppercase
+		$routeDefinedMethod = strtoupper($routeDefinedMethod);
+		$httpRequestMethodUsed = strtoupper($httpRequestMethodUsed);
+
+		return $routeDefinedMethod === $httpRequestMethodUsed;
 	}
 }
