3e651db4236217d99ac3d5611a303c45baa0c3f4
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-25 09:41:26 +0200

    TOURCMS-11746 remove hardcoded header redirections, added helper function to handle them

---
+++ src/helpers/RedirectionHelper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Helpers;
+
+class RedirectionHelper
+{
+	/**
+	 * Fires a header redirection to the specified location
+	 *
+	 * @param string $location The location to which the application will be redirected. Specify with left slashes /location/
+	 */
+	public static function headerRedirection(string $location): void
+	{
+		$redirectUrl = dirname($_SERVER['SCRIPT_NAME']) . $location;
+		header('Location: '. $redirectUrl .'');
+		exit;
+		
+	}
+}
