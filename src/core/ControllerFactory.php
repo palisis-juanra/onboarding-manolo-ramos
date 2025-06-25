6e3ce587b296c160f05cdb65d408b69e855c6221
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-25 17:24:03 +0200

    TOURCMS-11746 add controller factory

---
+++ src/core/ControllerFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Core;
+
+use Controllers\ChannelListController;
+use Controllers\SessionHandlerController;
+
+class ControllerFactory 
+{
+	// Controller names 
+	public const SESSION_HANDLER_CONTROLLER = 'SessionHandler';
+	public const CHANNEL_LIST_CONTROLLER = 'ChannelList';
+		
+	// Config passed on to the Controller instance
+	private $controllerDependencies;
+
+	public function __construct(array $controllerDependencies) 
+	{
+		$this->controllerDependencies = $controllerDependencies;
+	}
+
+	/**
+	 * Creates a controller based on the controllerName received.
+	 *
+	 * @return object an instance of the matched controller
+	 */
+	public function create(string $controllerName)
+	{
+		switch ($controllerName) {
+			case self::SESSION_HANDLER_CONTROLLER:
+				return new SessionHandlerController(
+					$this->controllerDependencies['redisClient'],
+					$this->controllerDependencies['templateRenderer'],
+				);
+			case self::CHANNEL_LIST_CONTROLLER:
+				return new ChannelListController(
+					...$this->controllerDependencies
+				);
+			default:
+				throw new \Exception("Controller '$controllerName' not found.");
+		}
+	}
+}
