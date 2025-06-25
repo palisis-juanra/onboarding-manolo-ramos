450471f2fafc99b07ba6eebb41edf925ed5b3644
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-25 17:23:51 +0200

    TOURCMS-11746 WIP updated app logic to handle controller factory

--- src/core/App.php
+++ src/core/App.php
@@ -2,15 +2,16 @@
 
 namespace Core;
 
-use Controllers\ChannelListController;
 use Core\Router;
-use Services\SessionHandlerService;
+use Controllers\SessionHandlerController;
 use Services\TourCMSClientService;
 use Services\TemplateRendererService;
 use Helpers\RedisInstanceHelper;
 class App
 {
+	// Core
 	private $router;
+	private $controllerFactory;
 
 	// Services
 	private $tourCMSclient;
@@ -18,8 +19,11 @@ class App
 	private $redisClient;
 
 	// Controllers
-	private $sessionHandlerService;
-	private $channelListController;
+	private $sessionHandlerController;
+	private $controllerInstance;
+
+	// Factory dependencies
+	private array $controllerDependencies;
 
 	public function __construct(array $envConfig)
 	{
@@ -28,28 +32,44 @@ class App
 		$this->templateRenderer = TemplateRendererService::getInstance();
 		$this->redisClient = RedisInstanceHelper::getInstance($envConfig['redis']);
 
-		// Initialize Controllers
-		$this->sessionHandlerService = new SessionHandlerService(
-			$this->redisClient,
-			$this->templateRenderer
-		);
-		// Initialize the Channel List Controller
-		$this->channelListController = new ChannelListController(
-			$this->tourCMSclient,
+		// Initialize Session Handler
+		$this->sessionHandlerController = new SessionHandlerController(
 			$this->redisClient,
 			$this->templateRenderer
 		);
 
+		// Build the controller dependencies collection
+		$this->controllerDependencies = [
+			'tourCMS' => $this->tourCMSclient,
+			'templateRenderer' => $this->templateRenderer,
+			'redisClient' => $this->redisClient
+		];
+
+		// Pass on the controller dependencies to the controller factory 
+		$this->controllerFactory = new ControllerFactory($this->controllerDependencies);
+
 		// Initialize Router
 		$this->router = new Router(
-			$this->sessionHandlerService,
-			$this->channelListController,
+			$this->sessionHandlerController,
 			$this->templateRenderer
 		);
 	}
 
 	public function run()
 	{
+		// Evaluate the route that is currently beign accessed.
 		$this->router->dispatch();
+		$routeInfo = $this->router->getRouteInfo();
+		// Check if there's an active session or if the current route doesn't require login
+		if ($this->sessionHandlerController->checkIfSessionIsActive() || !$routeInfo['requiresLogin']) {
+			$this->controllerInstance = $this->controllerFactory->create($routeInfo['controller']);
+			
+			// Run the associated function 
+			$this->controllerInstance->{$routeInfo['action']}();
+		} else {
+			// Load error template
+			$sessionController = $this->controllerFactory->create('SessionHandler');
+			$sessionController->renderNotFoundPage();
+		}
 	}
 }
