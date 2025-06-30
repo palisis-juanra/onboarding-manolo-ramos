6caa601dbf25f1672bc9edb502ef0c8ea2554272
Manolo Ramos <manolo.ramos@palisis.com>
2025-06-30 14:13:34 +0200

    TOURCMS-11747 updated error helper to fix error array search

--- src/helpers/ErrorHandlerHelper.php
+++ src/helpers/ErrorHandlerHelper.php
@@ -5,16 +5,24 @@ namespace Helpers;
 class ErrorHandlerHelper 
 {
 	protected static $errorMessages = [
-		"SESS_NO_CHANNEL_ID" => 'No Channel ID was found in the current session',
-		"POST_NO_CHANNEL_ID" => 'There was an error handling the selected Channel ID'
-
-
+		'CHANNELS' => [
+			'NO_CHANNEL_DATA' => 'No channel data available',
+			'SESS_NO_CHANNEL_ID' => 'No Channel ID was found in the current session',
+			'POST_NO_CHANNEL_ID' => 'There was an error handling the selected Channel ID'
+		],
+		'TOURS' => [
+			'NO_TOUR_DATA' => 'No tour data available'
+		]
 	];
 
 	public static function getErrorMessages(string $errorCode): string
 	{
-		$errorMessage = array_search($errorCode, self::$errorMessages);
+		foreach (self::$errorMessages as $type => $messages) {
+			if (array_key_exists($errorCode, $messages)) {
+				return $messages[$errorCode];
+			}
+		}
 
-		return $errorMessage;
+		return 'Unknown error code: ' . $errorCode;
 	}
 }
